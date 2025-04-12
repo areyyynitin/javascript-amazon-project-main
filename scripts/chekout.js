355a16fcceb62f1ba7a3668fe8bbaac34f020ade
areyyynitin <np089250@gmail.com>
2025-04-12 19:10:21 +0530

    import some function

--- scripts/chekout.js
+++ scripts/chekout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart , removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -38,7 +38,7 @@ cart.forEach((cartItem) => {
             <span class="update-quantity-link link-primary">
             Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary jsDeleteLink" data-product-id="${matchingProduct.id}">
             Delete
             </span>
         </div>
@@ -95,3 +95,11 @@ cart.forEach((cartItem) => {
 
 document.querySelector('.jsOrderSummary').innerHTML = cartSummaryHTML
 // console.log(cartSummaryHTML);
+
+document.querySelectorAll('.jsDeleteLink').forEach( (link) => {
+    link.addEventListener('click' , () => {
+        const productId =link.dataset.productId;
+        removeFromCart(productId);
+        console.log(cart);
+    });
+});
