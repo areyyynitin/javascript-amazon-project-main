cf7541c92a15201cf9dc4b553eac42bdbd094444
areyyynitin <np089250@gmail.com>
2025-04-14 22:49:06 +0530

    created automated test for formatCurrency

---
+++ test/moneyTest.js
@@ -0,0 +1,31 @@
+import {formatCurrency} from '../scripts/utils/money.js'
+if(formatCurrency(2095) === '20.95'){
+    console.log('passed')
+} else{
+    console.log('failed')
+
+}
+
+if(formatCurrency(0) === '0.00'){
+    console.log('passed')
+} else{
+    console.log('failed')
+
+}
+
+if(formatCurrency(2000.5) === '20.01'){
+    console.log('passed')
+} else{
+    console.log('failed')
+
+}
+
+if(formatCurrency(2000.4) === '20.00'){
+    console.log('passed')
+} else{
+    console.log('failed')
+
+}
+ 
+let ans = formatCurrency(2095)
+console.log(ans)
